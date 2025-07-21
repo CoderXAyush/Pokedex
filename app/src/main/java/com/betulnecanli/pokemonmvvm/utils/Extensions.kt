98a26c111cbce2e375c1d73958c936bf468d74d1
Kumar Ayush Aman <ayushhaman7@gmail.com>
2025-07-21 16:15:45 +0530

    Initial commit

--- app/src/main/java/com/betulnecanli/pokemonmvvm/utils/Extensions.kt
+++
@@ -1,9 +0,0 @@
-package com.betulnecanli.pokemonmvvm.utils
-
-
-fun String.findID() = this.substringAfter("pokemon").replace("/","").toInt()
-
-fun String.getImageOfPoke() : String {
-    val id = this.findID()
-    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
-}
